2b8ffbc6129ed4c8c4faf0881d48fae68831c753
Henrique Cavalléro <antoniocavallero935@gmail.com>
2026-01-06 18:47:48 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,19 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Department dp = new Department(1, "books");
-		
-		System.out.println(dp);
+
+		Seller seller = new Seller(1, "Alex", "alex@gmail.com", new Date(), 3000.0);
+
+		System.out.println(seller);
 
 	}
 
